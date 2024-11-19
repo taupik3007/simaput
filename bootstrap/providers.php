ccf78e220a1c00c9da074591c90c98a8e7cd3475
taupik3007 <taupikpathurrohman@gmail.com>
2024-11-19 11:20:58 +0700

    install spatie dan sweet alert

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,7 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
+    Spatie\Permission\PermissionServiceProvider::class,
+    RealRashid\SweetAlert\SweetAlertServiceProvider::class,
+
 ];
