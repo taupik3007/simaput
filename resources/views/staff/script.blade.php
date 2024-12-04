e1b1d30bf8e7a5695c42f831e3c8bb61e4e1d29a
taupik3007 <taupikpathurrohman@gmail.com>
2024-12-04 14:34:07 +0700

    memperbaiki migrattion user

--- resources/views/staff/script.blade.php
+++ resources/views/staff/script.blade.php
@@ -7,7 +7,7 @@
   <script src="{{asset('assets/js/theme/app.min.js')}}"></script>
   <script src="{{asset('assets/js/theme/sidebarmenu.js')}}"></script>
 
-  <!-- solar icons -->
+ <!-- solar icons -->
   {{-- <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script> --}}
 
   @stack('script')
